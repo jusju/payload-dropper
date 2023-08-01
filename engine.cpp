8d6a2767a8e226bde336fa2a9a70cc6ced0a9d1d
Jukka Juslin <jukka.juslin@haaga-helia.fi>
2023-08-01 19:17:30 +0300

    Update engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -4,27 +4,46 @@ int firstNegativeAttachmentPin = 0;
 int secondNegativeAttachmentPin = 2;
 int firstPositiveAttachmentPin = 4;
 int secondPositiveAttachmentPin = 7;
-int latchOpeningPin = 8;
-int sparePin1 = 10;
+int generalMinusInputPin = 8;
+int generalNegativeInputPin = 10;
 
 boolean initialStateMinusDefault = true;
 boolean initialStatePlusDefault = true;
-
+int upCounter = 0;
 
 void setup() {
   pinMode(firstNegativeAttachmentPin, OUTPUT);
   pinMode(secondNegativeAttachmentPin, OUTPUT);
   pinMode(firstPositiveAttachmentPin, OUTPUT);
   pinMode(secondPositiveAttachmentPin, OUTPUT);
-  pinMode(latchOpeningPin, OUTPUT);
-  pinMode(sparePin1, OUTPUT);
+  pinMode(generalMinusInputPin, OUTPUT);
+  pinMode(generalNegativeInputPin, OUTPUT);
+  digitalWrite(firstNegativeAttachmentPin, LOW);
+  digitalWrite(secondNegativeAttachmentPin, LOW);
+  digitalWrite(firstPositiveAttachmentPin, LOW);
+  digitalWrite(secondPositiveAttachmentPin, LOW);
+  digitalWrite(generalMinusInputPin, LOW);
+  digitalWrite(generalNegativeInputPin, LOW);
 }
 
 void loop() {
-  if(initialStateMinusDefault == true && initialStatePlusDefault == true) {
-    digitalWrite(firstNegativeAttachmentPin, HIGH);
-    digitalWrite(firstPositiveAttachmentPin, HIGH);
+  upCounter++;
+  if (initialStateMinusDefault == true && initialStatePlusDefault == true) {
+    digitalWrite(generalMinusInputPin, HIGH);
+    digitalWrite(generalNegativeInputPin, HIGH);
+    digitalWrite(firstNegativeAttachmentPin, HIGH);    
+    digitalWrite(firstPositiveAttachmentPin, HIGH);    
   }
- 
-  delay(2000);              
+  delay(1000);
+  if (initialStateMinusDefault == true && initialStatePlusDefault == true) {
+    digitalWrite(generalMinusInputPin, LOW);
+    digitalWrite(generalNegativeInputPin, LOW);
+    digitalWrite(firstNegativeAttachmentPin, LOW);    
+    digitalWrite(firstPositiveAttachmentPin, LOW);    
+  }
+  delay(1000);
+  
+  if(upCounter > 4) {
+    delay(30000);
+  }
 }
